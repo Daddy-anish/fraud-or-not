2768c3b78566fe66dcdd1a10711c67646c07bdbe
Grenish rai <ariestheracer@gmail.com>
2023-12-21 15:31:55 +0530

    added functionality

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 const toggleButton = document.getElementById("toggleButton");
-const togglePopup = document.getElementById("togglePopup");
-const toggleState = document.getElementById("toggleState");
-const closeButton = document.getElementById("closeButton");
+const messageDiv = document.getElementById("message");
+const URLdiv = document.getElementById("URL");
 
 toggleButton.addEventListener("change", function () {
   if (this.checked) {
-    toggleState.textContent = "The toggle is switched on.";
-    togglePopup.showModal();
+    messageDiv.textContent = "on";
   } else {
-    toggleState.textContent = "The toggle is switched off.";
+    messageDiv.textContent = "off";
   }
 });
 
-closeButton.addEventListener("click", function () {
-  togglePopup.close();
+// Query the current active tab in the current window
+// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//     // Set the text content of the HTML element with ID 'URLdiv' to the URL of the active tab
+//     URLdiv.textContent = tabs[0].url;
+// });
+
+toggleButton.addEventListener("change", function () {
+  if (this.checked) {
+    const urlInfo = window.location.href;
+    URLdiv.textContent = urlInfo;
+  }
 });
